f312ba6d1e84af4b6916228d4f0633981f7bfc92
Anton Charchenko <acharchenko@netlabsystems.com>
2012-03-01 14:27:49 +0200

    correct rename files

--- translate_yandex.sh
+++ translate_yandex.sh
@@ -6,9 +6,12 @@ _zenity="/usr/bin/zenity"
 text=$(${_zenity} --title  "Enter text" --entry --entry-text="$(xclip -o)" )
 #text=$(${_zenity} --title "Enter text" --entry --entry-text="$(xsel -o -p)" )
 
+sLNG=$1
+tLNG=$2
+
 if [ $? -eq 0 ]
 then
-wget -qO- translate.yandex.ru/tr.json/translate --post-data="srv=tr-text&lang=en-ru&text=$text" | sed 's/"//g' | zenity --width=600 --height=240 --title "Translation for $text" --text-info
+wget -qO- translate.yandex.ru/tr.json/translate --post-data="srv=tr-text&lang=$sLNG-$tLNG&text=$text" | sed 's/"//g' | zenity --width=600 --height=240 --title "Translation for $text" --text-info
 
 # wget -O ${_out} http://translate.yandex.ru/tr.json/translate --post-data="srv=tr-text&lang=en-ru&text=$text"
 #  sed -i 's/"//g' ${_out}
